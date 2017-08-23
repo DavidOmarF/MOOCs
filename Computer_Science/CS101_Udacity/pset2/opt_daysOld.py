5a50afa4ef13e2eed33c716da391858b744684be
DavidOmarF <DavidOmarFCh@gmail.com>
2017-08-23 01:25:34 -0500

    optional2: Days old (tests removed)

--- Computer_Science/CS101_Udacity/pset2/opt_daysOld.py
+++ Computer_Science/CS101_Udacity/pset2/opt_daysOld.py
@@ -8,39 +8,9 @@
 #
 
 
-## Number -> Boolean
-## Takes a number representing a year, and returns true if its leap, and false if not.
-## def isLeap(y): return True                                              #stub
-def isLeap(y):
-    ## Is leap if is divisible by 4 and not by 100. Unless is divisible by 400.
-    return (y % 4 == 0) and (y % 100 != 0) or (y % 400 == 0)
-
-
-## Number Number -> Number
-## Takes two years, and returns the number of days between them. Assuming same MM and DD
-## def daysBtwYears(y1, y2): return 0                                      #stub
-def daysBtwYears(y1, y2):
-    d = 0
-    d = (y2 - y1) * 365
-    for i in range (y1, y2+1):
-        if isLeap(i):
-            d = d + 1
-    return d
-
-## Number Number -> Number
-## Takes two months and returns the number of days between first day of m1, and last of m2
-##def daysBtwMonths(m1, m2): return 0                                     #stub
-def daysBtwMonths(m1, m2):
-    monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    d = 0
-    for i in range (m1 - 1, m2 - 1):
-        d = d + monthDays[i]
-    if m2 < m1: return 0 - d
-    return d
-
 ## Number Number Number Number Number Number -> Number
 ## Takes dates (y1, m1, d1) and (y2, m2, d2) and returns (2) - (1) in days.
-## def daysBetweenDates(y1, m1, d1, y2, m2, d2): return 0               #stub
+## def daysBetweenDates(y1, m1, d1, y2, m2, d2): return 0                   #stub
 def daysBetweenDates(y1, m1, d1, y2, m2, d2):
     days = 0
     ## Add days between y1 and y2
@@ -64,19 +34,33 @@ def daysBetweenDates(y1, m1, d1, y2, m2, d2):
     return days
 
 
-# Test routine
-def test():
-    test_cases = [((2012,1,1,2012,2,28), 58), 
-                  ((2012,1,1,2012,3,1), 60),
-                  ((2011,6,30,2012,6,30), 366),
-                  ((2011,1,1,2012,8,8), 585 ),
-                  ((1900,1,1,1999,12,31), 36523)]
-    for (args, answer) in test_cases:
-        result = daysBetweenDates(*args)
-        if result != answer:
-            print ("Test with data:", args, "failed")
-            print ("        ", result, " vs. ", answer)
-        else:
-            print ("Test case passed!")
+## Number -> Boolean
+## Takes a number representing a year, and returns true if its leap, and false if not.
+## def isLeap(y): return True                                              #stub
+def isLeap(y):
+    ## Is leap if is divisible by 4 and not by 100. Unless is divisible by 400.
+    return (y % 4 == 0) and (y % 100 != 0) or (y % 400 == 0)
 
-test()
+
+## Number Number -> Number
+## Takes two years, and returns the number of days between them. Assuming same MM and DD
+## def daysBtwYears(y1, y2): return 0                                      #stub
+def daysBtwYears(y1, y2):
+    d = 0
+    d = (y2 - y1) * 365
+    for i in range (y1, y2+1):
+        if isLeap(i):
+            d = d + 1
+    return d
+
+## Number Number -> Number
+## Takes two months and returns the number of days between m1 and m2-1
+##def daysBtwMonths(m1, m2): return 0                                     #stub
+def daysBtwMonths(m1, m2):
+    monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    d = 0
+    for i in range (m1 - 1, m2 - 1):
+        d = d + monthDays[i]
+    ## if m2 happens before m1, return a negative number
+    if m2 < m1: return 0 - d
+    return d
