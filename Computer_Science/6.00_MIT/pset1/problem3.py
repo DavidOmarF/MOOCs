8ce52ff33e8333879e3e9ee44915e8a59ef25fbb
davidomarf <davidomarfch@gmail.com>
2017-09-15 06:13:20 -0500

    something

--- Computer_Science/6.00_MIT/pset1/problem3.py
+++ Computer_Science/6.00_MIT/pset1/problem3.py
@@ -1,5 +1,6 @@
-maxLenght = "azcbobobegghakl"
-count = 0;
-for i in maxLenght:
-    print (i )
+s = "azcbobobegghakl"
+indexes = []
+lenghts = []
+for i in range(1, len(s)):
+    if (s[i])
 print(count)
