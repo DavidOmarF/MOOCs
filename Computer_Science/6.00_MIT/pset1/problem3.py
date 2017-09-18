394268e8998798c075c62abe6bfe6c2d1b0d2d36
davidomarf <davidomarfch@gmail.com>
2017-09-17 19:24:01 -0500

    first set

--- Computer_Science/6.00_MIT/pset1/problem3.py
+++ Computer_Science/6.00_MIT/pset1/problem3.py
@@ -1,6 +1,18 @@
-s = "azcbobobegghakl"
-indexes = []
-lenghts = []
+s = "kxnxiixoarjmgutrw"
+indexes = [0]
+lengths = []
+words = []
+j = 0
 for i in range(1, len(s)):
-    if (s[i])
-print(count)
+    if ord(s[i]) < ord(s[i - 1]):
+        indexes.append(i)
+        lengths.append(i - indexes[j])
+        words.append(s[indexes[j]:i])
+        j = j + 1
+    elif i + 1 == len(s):
+        lengths.append(i - indexes[j] + 1)
+        words.append(s[indexes[j]:i+1])
+
+longest_i = [i for i, x in enumerate(lengths) if x == max(lengths)]
+longest = [words[index] for index in longest_i]
+print(longest[0])
